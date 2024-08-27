aa356efeab1d435bc32698c3f568cc6a926586c9
Francis <francism4402@gmail.com>
2024-08-28 05:44:15 +0600

    loading model update

--- src/Components/ModelView.jsx
+++ src/Components/ModelView.jsx
@@ -4,8 +4,9 @@ import Lights from "./Lights";
 import { Suspense } from "react";
 import Iphone from "./Iphone";
 import * as THREE from 'three';
+import Loader from "./Loader";
 
-const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
+const ModelView = ({index, groupRef, gsapType, controlRef, setRotationState, size, item}) => {
   return (
     <View index={index} id={gsapType} className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}>
       <ambientLight intensity={0.3} />
@@ -17,7 +18,7 @@ const ModelView = ({index, groupRef, gsapType, controlRef, setRotationSize, size
       <OrbitControls makeDefault ref={controlRef} enableZoom={false} enablePan={false} rotateSpeed={0.4} target={new THREE.Vector3(0, 0, 0)} onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())} />
 
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0,0,0]}>
-        <Suspense fallback={<Html><div>Loading</div></Html>}>
+        <Suspense fallback={<Loader />}>
           <Iphone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} item={item} size={size} />
         </Suspense>
       </group>
